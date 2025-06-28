b109795592bbbab7d55d270a7287f2625e45f2bf
宇田 <110873087+yutian0525@users.noreply.github.com>
2025-06-28 21:22:50 +0800

    111

--- src/main/java/com/soft/config/CorsConfig.java
+++ src/main/java/com/soft/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 允许所有路径
-                .allowedOrigins("http://localhost:8080") // 允许来自前端的域名
+                .allowedOrigins("https://aaa.yutiantian.top/") // 允许来自前端的域名
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的HTTP方法
                 .allowedHeaders("*") // 允许所有请求头
                 .allowCredentials(true) // 允许发送Cookie等凭证
